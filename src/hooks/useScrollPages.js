d60edcabecb0a68611aaab096c804e14a150bfa9
Saypi <kurbanovsajp@gmail.com>
2024-09-21 13:25:47 +0300

    upd

--- src/hooks/useScrollPages.js
+++ src/hooks/useScrollPages.js
@@ -7,8 +7,10 @@ export default function useScrollPages() {
     const [scroll, setScroll] = useState(0)
     const isBlocked = useRef(false)
     const [block, setBlock] = useState(false)
+    const [isDrag, setIsDrag] = useState(false)
 
     const timer = useRef(null)
+    const mainPage = useRef(null)
     const count = 9
   
     Store.useListener('change_page_header', (data) => {
@@ -18,64 +20,71 @@ export default function useScrollPages() {
 
     Store.useListener('block_scroll', setBlock)
     
-    const wheelFunction = (e) => {
-      // if (window.innerWidth > 992 && !isBlocked.current && !block) {
-        //   const wheel = e.deltaY;
-        //   console.log(wheel)
-        //   if(wheel > 0) {
-          //     setScroll(prev => {
-            //       let result = prev >= (100 * (count - 1)) ? prev : prev + 5
-      //       if(result >= (100 * page)) {
-        //         setPage(prev => prev + 1);
-        //         Store.setListener('change_page_from_scroll', (page + 1))
-        //       }
-        //       return result
-        //     });
-        //   } else {
-          //     setScroll(prev => {
-            //       let result = prev <= 1 ? prev : prev - 5
-            //       if(((100 * (page - 1)) - 80) > result) {
-              //         setPage(prev => prev - 1);
-              //         Store.setListener('change_page_from_scroll', (page - 1))
-              //       }
-              //       return result
-              //     })
-              //   }
-              //}
+const wheelFunction = (e) => {
+
+  if (window.innerWidth > 992 && !isBlocked.current && !block) {
+
+      let wheel = e.deltaY > 0 ? Math.min(e.deltaY, 50) : Math.max(e.deltaY, -50);
+      wheel *= 0.05
+
+
+      if(wheel > 0) {
+
+        setScroll(prev => {
+          let result = prev >= (100 * (count - 1)) ? prev : prev + wheel
+          if(result >= (100 * page)) {
+            setPage(prev => prev + 1);
+            Store.setListener('change_page_from_scroll', (page + 1))
+          }
+          return result
+        });
+      } else {
+        setScroll(prev => {
+          let result = prev <= 1 ? prev : prev + wheel
+          if(((100 * (page - 1)) - 80) > result) {
+            setPage(prev => prev - 1);
+            Store.setListener('change_page_from_scroll', (page - 1))
+            return page * 100
+          }
+          return result
+        })
+      }
+  }
+}
               
       //e.preventDefault()
-      if (window.innerWidth > 991 && !isBlocked.current && !block) {
-        let wheel = e.deltaY > 0 ? Math.min(e.deltaY, 50) : Math.max(e.deltaY, -50);
-        wheel *= 0.05
+    //   if (window.innerWidth > 991 && !isBlocked.current && !block) {
+    //     let wheel = e.deltaY > 0 ? Math.min(e.deltaY, 50) : Math.max(e.deltaY, -50);
+    //     wheel *= 0.05
 
-          if(wheel > 0) {
-            if(((100 * page) - 20) < scroll) {
-              setScroll(100 * page);
-              setPage(prev => prev + 1);
-              Store.setListener('change_page_from_scroll', (page + 1))
-              isBlocked.current = true;
-              timer.current = setTimeout(() => { 
-                isBlocked.current = false;
-              }, 1000);
-            } else {
-              setScroll(prev => prev >= (100 * (count - 1)) ? prev : prev + wheel);
-            }
+    //       if(wheel > 0) {
+    //         if(((100 * page) - 20) < scroll) {
+    //           setScroll(100 * page);
+    //           setPage(prev => prev + 1);
+    //           Store.setListener('change_page_from_scroll', (page + 1))
+    //           isBlocked.current = true;
+    //           timer.current = setTimeout(() => { 
+    //             isBlocked.current = false;
+    //           }, 1000);
+    //         } else {
+    //           setScroll(prev => prev >= (100 * (count - 1)) ? prev : prev + wheel);
+    //         }
 
-          } else {
-            if(((100 * (page - 1)) - 80) > scroll) {
-              setScroll(100 * (page - 2));
-              setPage(prev => prev - 1);
-              Store.setListener('change_page_from_scroll', (page - 1))
-              isBlocked.current = true;
-              timer.current = setTimeout(() => { 
-                isBlocked.current = false;
-              }, 1000)
-            } else {
-              setScroll(prev => prev <= 1 ? prev : prev + wheel)
-            }
-          }
-        }
-    }; 
+    //       } else {
+    //         if(((100 * (page - 1)) - 80) > scroll) {
+    //           setScroll(100 * (page - 2));
+    //           setPage(prev => prev - 1);
+    //           Store.setListener('change_page_from_scroll', (page - 1))
+    //           isBlocked.current = true;
+    //           timer.current = setTimeout(() => { 
+    //             isBlocked.current = false;
+    //           }, 1000)
+    //         } else {
+    //           setScroll(prev => prev <= 1 ? prev : prev + wheel)
+    //         }
+    //       }
+    //     }
+    // }; 
 
     useEffect(() => {
       return () => {
@@ -101,5 +110,37 @@ export default function useScrollPages() {
     //     };
     // }, []);
 
-    return {isBlocked, scroll, page, wheelFunction}
+    const dragDown = (e) => {
+      e.preventDefault()
+      if(mainPage.current) {
+        mainPage.current.style.cursor = 'grabbing';
+      }
+      setIsDrag(true)
+    }
+
+    const dragMove = (e) => {
+      e.preventDefault()
+      if(isDrag) {
+        setScroll(prev => {
+          let result = prev - e.movementX * 0.08
+          if(result <= 0) {
+            return 0
+          } else if (result >= (100 * (count - 1))) {
+            return 100 * (count - 1)
+          } else {
+            return result
+          }
+        });
+      }
+    }
+
+    const dragEnd = (e) => {
+      e.preventDefault()
+      if(mainPage.current) {
+        mainPage.current.style.cursor = 'default';
+      }
+      setIsDrag(false)
+    }
+
+    return {isBlocked, scroll, page, wheelFunction, dragDown, dragEnd, dragMove, mainPage}
 }
